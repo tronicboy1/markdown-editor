651ea3ae118db24c015d4a3b6d9e5c9d3a670c40
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-04 09:03:36 +0900

    add shortcuts to save and open buttons

--- src/application/renderer.ts
+++ src/application/renderer.ts
@@ -13,6 +13,22 @@ editor.addEventListener("input", () => {
   });
 });
 
+const saveButton = document.querySelector("button#save")!;
+const saveImagesButton = document.querySelector("button#save-images")!;
+const openButton = document.querySelector<HTMLButtonElement>("button#open")!;
+document.addEventListener("keydown", event => {
+  const isMeta = event.metaKey;
+  const shiftPressed = event.shiftKey;
+  const isDkey = event.key.toUpperCase() === "D";
+  const isOkey = event.key.toUpperCase() === "O";
+  if (!isMeta) return;
+  if (isOkey) return openButton.dispatchEvent(new Event("click"));
+  if (isDkey && shiftPressed)
+    return saveImagesButton.dispatchEvent(new Event("click"));
+  if (isDkey) return saveButton.dispatchEvent(new Event("click"));
+  return;
+});
+
 const cache = window.localStorage.getItem("cache");
 if (cache) {
   editor.innerHTML = cache;
