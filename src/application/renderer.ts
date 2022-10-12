58d077c7c57e90cac1e33a61af48b47859e323de
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-12 16:29:42 +0900

    refactor keyboard shortcuts to seperate script

--- src/application/renderer.ts
+++ src/application/renderer.ts
@@ -2,8 +2,9 @@ import "lit-markdown-editor";
 import { tagName as modalTagName } from "./components/base-modal";
 import "./scripts/save-buttons";
 import "./scripts/open-button";
+import "./scripts/keyboard-shortcuts"
 import { renderMarkdown } from "./helpers";
-import { filter, fromEvent, map, sampleTime } from "rxjs";
+import { fromEvent, map, sampleTime } from "rxjs";
 import { LitMarkdownEditor } from "lit-markdown-editor";
 
 const root = document.getElementById("root")!;
@@ -25,22 +26,6 @@ editorInput$
       article.innerHTML = rawHTML;
     });
   });
-
-const saveButton = document.querySelector("button#save")!;
-const saveImagesButton = document.querySelector("button#save-images")!;
-const openButton = document.querySelector<HTMLButtonElement>("button#open")!;
-const keydown$ = fromEvent<KeyboardEvent>(document, "keydown");
-const metaKeyPressed$ = keydown$.pipe(filter(event => event.metaKey));
-metaKeyPressed$
-  .pipe(filter(event => event.key.toUpperCase() === "D"))
-  .subscribe(event => {
-    const shiftPressed = event.shiftKey;
-    if (shiftPressed) return saveImagesButton.dispatchEvent(new Event("click"));
-    return saveButton.dispatchEvent(new Event("click"));
-  });
-metaKeyPressed$
-  .pipe(filter(event => event.key.toUpperCase() === "O"))
-  .subscribe(() => openButton.dispatchEvent(new Event("click")));
 
 const cache = window.localStorage.getItem("cache");
 if (cache) {
---
+++ src/application/scripts/keyboard-shortcuts.ts
@@ -0,0 +1,17 @@
+import { filter, fromEvent } from "rxjs";
+
+const saveButton = document.querySelector("button#save")!;
+const saveImagesButton = document.querySelector("button#save-images")!;
+const openButton = document.querySelector<HTMLButtonElement>("button#open")!;
+const keydown$ = fromEvent<KeyboardEvent>(document, "keydown");
+const metaKeyPressed$ = keydown$.pipe(filter(event => event.metaKey));
+metaKeyPressed$
+  .pipe(filter(event => event.key.toUpperCase() === "D"))
+  .subscribe(event => {
+    const shiftPressed = event.shiftKey;
+    if (shiftPressed) return saveImagesButton.dispatchEvent(new Event("click"));
+    return saveButton.dispatchEvent(new Event("click"));
+  });
+metaKeyPressed$
+  .pipe(filter(event => event.key.toUpperCase() === "O"))
+  .subscribe(() => openButton.dispatchEvent(new Event("click")));
