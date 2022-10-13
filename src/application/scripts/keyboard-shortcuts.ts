811e8b430a882a4db9d720cb72a67867ece4f959
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-13 09:28:59 +0900

    update markdown-editor

--- src/application/scripts/keyboard-shortcuts.ts
+++ src/application/scripts/keyboard-shortcuts.ts
@@ -6,17 +6,21 @@ const openButton = document.querySelector<HTMLButtonElement>("button#open")!;
 const keydown$ = fromEvent<KeyboardEvent>(document, "keydown");
 const metaKeyPressed$ = keydown$.pipe(filter(event => event.metaKey));
 metaKeyPressed$
-  .pipe(filter(event => event.key.toUpperCase() === "D"))
+  .pipe(filter(event => event.key.toUpperCase() === "S"))
   .subscribe(event => {
+    event.preventDefault();
     const shiftPressed = event.shiftKey;
     if (shiftPressed) return saveImagesButton.dispatchEvent(new Event("click"));
     return saveButton.dispatchEvent(new Event("click"));
   });
 metaKeyPressed$
   .pipe(filter(event => event.key.toUpperCase() === "O"))
-  .subscribe(() => openButton.dispatchEvent(new Event("click")));
+  .subscribe((event) => {
+    event.preventDefault();
+    openButton.dispatchEvent(new Event("click"));
+  });
 metaKeyPressed$
   .pipe(filter(event => event.key.toUpperCase() === "R"))
   .subscribe(event => {
-    event.preventDefault();
+    event.preventDefault(); // dont let the keyboard shortcut reload the page
   });
