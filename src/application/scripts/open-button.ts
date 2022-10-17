99576fbc1dbead4f21793f6498a2636d377f23f7
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-17 14:46:06 +0900

    use File System Access API

--- src/application/scripts/open-button.ts
+++ src/application/scripts/open-button.ts
@@ -2,28 +2,36 @@ import loadZipFile from "../helpers/load-zip";
 
 const editor = document.querySelector("lit-markdown-editor")!;
 const openButton = document.querySelector<HTMLButtonElement>("button#open")!;
-const hiddenInput = document.querySelector<HTMLInputElement>(
-  "input#markdown-input"
-)!;
 
-const handleOpenClick: EventListener = () => hiddenInput.click();
-
-const handleFileInput: EventListener = event => {
-  const files = hiddenInput.files;
-  if (!files) throw TypeError("Listener must be used with file input.");
-  const [file] = Array.from(files);
-  if (!(file && file.size)) return;
-  const regex = /.*\.md$/;
-  if (regex.test(file.name)) {
-    return file.text().then(markdown => {
-      if (!markdown.length) return;
-      editor.value = markdown;
+const handleOpenClick: EventListener = () => {
+  const pickerOpts: OpenFilePickerOptions = {
+    types: [
+      {
+        accept: {
+          "text/plain": [".md"],
+          "application/zip": [".zip"],
+        },
+      },
+    ],
+    excludeAcceptAllOption: true,
+    multiple: false,
+  };
+  window
+    .showOpenFilePicker(pickerOpts)
+    .then(([fileHandler]) => fileHandler.getFile())
+    .then(file => {
+      if (!(file && file.size)) return;
+      const regex = /.*\.md$/;
+      if (regex.test(file.name)) {
+        return file.text().then(markdown => {
+          if (!markdown.length) return;
+          editor.value = markdown;
+        });
+      }
+      loadZipFile(file).then(markdown => {
+        editor.value = markdown;
+      });
     });
-  }
-  loadZipFile(file).then(markdown => {
-    editor.value = markdown;
-  });
 };
 
 openButton.addEventListener("click", handleOpenClick);
-hiddenInput.addEventListener("input", handleFileInput);
